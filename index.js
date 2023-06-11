1ac51ceeeecbca78415dbe358cf0d0d7d5cc41fa
amdpt323 <samandeep21@iitk.ac.in>
2023-06-12 00:31:39 +0530

    added nodemailer and implemented sending OTP

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-require('dotenv').config({path: './config/env.js'}); 
-const express = require('express');
-const app = express();
-const port = process.env.PORT; 
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
 
 // Basic route
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-
+  res.send('Hello, World!')
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+  console.log(`Server is running on http://localhost:${port}`)
+})
