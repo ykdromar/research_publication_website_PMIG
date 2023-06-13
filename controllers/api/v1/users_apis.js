fc81eb9305bdec9c7cbd773b7c14d178c7ae8389
Lavesh Gupta <lavesh.gupta@hudsonthames.org>
2023-06-13 21:35:27 +0530

    Add user schema and controllers

---
+++ controllers/api/v1/users_apis.js
@@ -0,0 +1,49 @@
+const User = require("../models/User");
+const { sendOTP } = require("../config/nodemailerConfig.js");
+
+exports.createUser = async (req, res) => {
+    const { username } = req.body;
+  
+    try {
+      // Generate OTP and send it via email
+      const otp = await sendOTP(username + "@iitk.ac.in");
+  
+      // Create and save the user in the database
+      const newUser = new User({
+        username,
+        isVerified: false,
+        OTP: otp,
+      });
+      await newUser.save();
+  
+      res.status(200).send("User created!");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Failed to create user!");
+    }
+  };
+  
+
+exports.verifyOTP = async (req, res) => {
+    const { username, otp } = req.body;
+  
+    try {
+      // Find the user in the database
+      const user = await User.findOne({ username });
+  
+      // Check if user exists and OTP matches
+      if (!user || user.OTP !== otp) {
+        res.status(400).send("Invalid OTP or username.");
+        return;
+      }
+  
+      // Update isVerified to true
+      user.isVerified = true;
+      await user.save();
+  
+      res.status(200).send("OTP verified successfully.");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Failed to verify OTP.");
+    }
+  };
