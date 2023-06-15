975fb87aef73cdade49657b98b4ffa68fc546a47
Lavesh Gupta <lavesh.gupta@hudsonthames.org>
2023-06-15 14:55:05 +0530

    Add JWT Passport Strategy for authentication

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const env = require("./config/env");
 const mongoose = require("./config/mongoose");
 const app = express();
 const port = env.PORT;
-
+const passport = require('passport'); 
 // Basic route
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.use(passport.initialize())
+
+
+// Configure Passport to use the JWT strategy
+passportConfig(passport);
+
+// Register authentication routes
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
