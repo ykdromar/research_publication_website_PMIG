c0e7d2b5b5760f7ae3b362add6cd50e6d24d53f7
amdpt323 <samandeep21@iitk.ac.in>
2023-06-17 22:26:07 +0530

    added login api

--- controllers/api/v1/authController.js
+++ controllers/api/v1/authController.js
@@ -11,12 +11,33 @@ const generateToken = (user) => {
 
 const login = async (req, res) => {
 
-  try {
-    const user = { _id: "user-id", username: "username" };
+    try {
+    const { username, password } = req.body
+    if (!username || !password) {
+      res.status(401).send('Please Provide Username and Password')
+      return
+    }
 
-    const token = generateToken(user);
+    const user = await User.findOne({ username })
+    if (!user) {
+      res.status(401).send('Invalid Username')
+      return
+    } else if (!user.isVerified) {
+      res.status(400).send('Please Verify OTP')
+      return
+    }
 
-    res.status(200).json({ token });
+    // a method created in user to compare the provided password in the req body and stored password in db
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+      res.status(401).send('Invalid Credentials')
+      return
+    }
+
+    //a jwt should be created and should be sent as response
+    const token = generateToken(user)
+
+    res.status(200).json({ token })
   } catch (error) {
     console.log(error);
     res.status(500).send("Failed to login.");
